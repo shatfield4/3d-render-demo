56b64e48034f4250c6c032a8e5573ffd1bff8155
shatfield4 <seanhatfield5@gmail.com>
2023-03-21 22:07:51 -0700

    changed powered by openai button

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
-        <footer className="flex items-center justify-center  border-neutral-800 w-full h-16">
+        <footer className="flex items-center justify-center border-neutral-800 w-full h-16">
         <a
-          className="flex items-center justify-center bg-black text-white px-4 py-2 rounded-md text-sm font-medium"
+          className="flex items-center justify-center bg-green-600 hover:bg-green-500 text-white px-4 py-2 rounded-md text-sm font-medium"
           href="https://openai.com"
           target="_blank"
           rel="noopener noreferrer"
